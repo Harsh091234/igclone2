0e143a3e84f3b692a74d2b53c1a8eae0fbaf425a
Harsh Sharma <hsharma4090@gmail.com>
2026-04-16 13:47:33 +0530

    Refactor ProtectedRoutes for email verification logic

--- client/src/routes/ProtectedRoutes.tsx
+++ client/src/routes/ProtectedRoutes.tsx
@@ -4,12 +4,8 @@ import { useSelector } from "react-redux";
 export const ProtectedRoutes = ({ children }: { children?: React.ReactNode }) => {
   const user = useSelector((state: any) => state.auth.user);
   const location = useLocation();
-const isVerifyEmailPage = location.pathname.startsWith("/verify-email");
-  const isVerificationRoute =
-    location.pathname.startsWith("/check-email") ||
-    location.pathname.startsWith("/resend-verification-url");
 
-  // ⏳ still loading user
+  // ⏳ still loading
   if (user === undefined) return null;
 
   // ❌ Not logged in
@@ -17,24 +13,14 @@ const isVerifyEmailPage = location.pathname.startsWith("/verify-email");
     return <Navigate to="/login" replace />;
   }
 
-  // ❌ Not verified → allow ONLY verification pages
+  // ❌ Not verified
   if (!user.isEmailVerified) {
-    if (isVerifyEmailPage || isVerificationRoute ) {
-      return children ? children : <Outlet />;
-    }
     return <Navigate to="/check-email" replace />;
   }
 
-  // ✅ Already verified → block verification pages
-  if (user.isEmailVerified && isVerificationRoute && !isVerifyEmailPage) {
-    return <Navigate to="/" replace />;
-  }
-
   // ❌ Not onboarded
-  if (!user.isProfileComplete) {
-    if (location.pathname !== "/onboarding"  &&  !isVerifyEmailPage) {
-      return <Navigate to="/onboarding" replace />;
-    }
+  if (!user.isProfileComplete && location.pathname !== "/onboarding") {
+    return <Navigate to="/onboarding" replace />;
   }
 
   // ✅ Already onboarded → block onboarding page
