fb7d0b2b4e6fa8c641ab3a0e8949d0886e3f8f4b
Harsh Sharma <hsharma4090@gmail.com>
2026-04-16 10:44:43 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "./utils/ThemeProvider.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 
-createRoot(document.getElementById("root")!).render
+createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Provider store={store}>
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
