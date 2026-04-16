c71ba9f948d2d2ed5b92fb147f0427cda2142b43
Harsh Sharma <hsharma4090@gmail.com>
2026-04-16 10:41:09 +0530

    Remove ClerkProvider and publishable key validation
    Removed ClerkProvider and related publishable key check from main.tsx.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ClerkProvider } from "@clerk/clerk-react";
 
 import { Toaster } from "react-hot-toast";
 
@@ -11,14 +10,7 @@ import { ThemeProvider } from "./utils/ThemeProvider.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
-
-createRoot(document.getElementById("root")!).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+createRoot(document.getElementById("root")!).render
     <BrowserRouter>
       <Provider store={store}>
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
@@ -50,5 +42,5 @@ createRoot(document.getElementById("root")!).render(
         reverseOrder={false}
       />
     </BrowserRouter>
-  </ClerkProvider>,
+
 );
