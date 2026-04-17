89474e6435c9393f7de3a76e495732fc1a442d06
Harsh Sharma <hsharma4090@gmail.com>
2026-04-17 16:14:57 +0530

    Refactor CheckEmailPage by removing user logic
    Removed unused user selector and related links.

--- client/src/pages/CheckEmailPage.tsx
+++ client/src/pages/CheckEmailPage.tsx
@@ -1,12 +1,11 @@
 
 import { Link } from "react-router-dom";
 import LeftSectionStartPage from "../components/LeftSectionStartPage";
-import { useSelector } from "react-redux";
-import type { RootState } from "../store/store";
+
 
 
 export default function CheckEmailPage() {
-  const user = useSelector((state: RootState) => state.auth.user);
+  
    
   
     
@@ -18,7 +17,7 @@ export default function CheckEmailPage() {
         <p className="text-muted-foreground text-lg">
           We have sent a verification link to your email.
         </p>
-
+        <span className="text-sm text-muted-foreground">or</span>
         <Link to="/resend-verification-url"className="
       relative inline-block text-foreground
       after:content-['']
@@ -34,26 +33,6 @@ export default function CheckEmailPage() {
           Resend email
         </Link>
 
-
-        <span className="text-sm text-muted-foreground">or</span>
-
-  <Link
-    to={user ? "/" : "/login"}
-    className="
-      relative inline-block text-foreground
-      after:content-['']
-      after:absolute after:h-[1.5px] after:w-full
-      after:bg-foreground
-      after:-bottom-0.5 after:right-0
-      after:scale-x-0
-      after:origin-right
-      after:transition-transform after:duration-300
-      hover:after:scale-x-100
-      hover:after:origin-left
-    "
-  >
-    {user ? "Back to Home" : "Back to Login"}
-  </Link>
       </div>
     </div>
   );
