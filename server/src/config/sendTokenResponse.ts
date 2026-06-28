b6d574dbb7fa4e12b2dbb89f0f60be61b7645981
Harsh091234 <hsharma4090@gmail.com>
2026-06-28 13:12:57 +0530

    d

--- server/src/config/sendTokenResponse.ts
+++ server/src/config/sendTokenResponse.ts
@@ -42,19 +42,17 @@ export const sendTokenResponse = async (
   });
 
   const accessCookieOptions = {
-    expires: new Date(Date.now() + 15 * 60 * 1000), // 15 min
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 1 * 24 * 60 * 60 * 1000,
   };
 
   const refreshCookieOptions = {
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    path: "/",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
   const safeUser = sanitizeUser(user);
